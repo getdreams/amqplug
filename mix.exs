4000ab9ccca23e52babb3b73554cec7c6bd6165f
Kiril Videlov <kiril.vi@runbox.no>
2017-05-26 09:02:04 +0200

    Adds amqp dependency

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Amqplug.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :amqp]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,8 @@ defmodule Amqplug.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:amqp, "~> 0.2.1"}
+    ]
   end
 end
